3ad1e73b46f4288a5d9820c0686880f7e7f42ec8
levi <levi@localhost.localdomain>
2020-02-17 16:05:35 +0800

    更新

--- service/ws_server.lua
+++ service/ws_server.lua
@@ -9,17 +9,19 @@ local ws_server_id = -1
 
 local agents = {}
 
-local command = {}
+local command = {
+    server_id = -1
+}
 
 function command.START(port)
     local protocol = "ws"
-    ws_server_id = socket.listen("0.0.0.0", port)
-    assert(ws_server_id ~= -1, "listten fail")
+    command.server_id = socket.listen("0.0.0.0", port)
+    assert(server_id ~= -1, "listten fail")
 
     skynet.error(string.format("Listen websocket port:" .. port .. " protocol:%s", protocol))
 
     socket.start(
-        ws_server_id,
+        command.server_id,
         function(id, addr)
             print(string.format("accept client socket_id: %s addr:%s", id, addr))
 
@@ -28,12 +30,12 @@ function command.START(port)
             skynet.send(handle_id, "lua", id, protocol, addr)
         end
     )
-    
+
     return 0, "websocket server start success"
 end
 
 function command.STOP()
-    socket.close(ws_server_id)
+    socket.close(command.server_id)
     skynet.error("websocket exit")
 end
 
@@ -47,8 +49,12 @@ skynet.start(
                     local f = command[cmd]
                     assert(f)
                     skynet.ret(skynet.pack(f(...)))
+                elseif cmd == "STOP" then
+                    local f = command[cmd]
+                    assert(f)
+                    skynet.ret(skynet.pack(f(...)))
                 else
-                    skynet.error(string.format("Unknown command %s", tostring(cmd)))
+                    skynet.error(string.format("unknown command %s", tostring(cmd)))
                 end
             end
         )
